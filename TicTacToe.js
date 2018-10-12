ca8ef2f5dcfba3e5e608889e375263e9b48d585e
Alexander Lee <alixlii@Alexanders-MacBook-Air.local>
2018-10-12 09:57:57 -0700

    Added board layout

---
+++ TicTacToe.js
@@ -0,0 +1,6 @@
+var board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function boardLayout() {
+  console.log(" " + board[0] + " " + "|" + " " + board[1] + " " + "|" + " " + board[2] + " " + "\n" + "-----------\n" + " " + board[3] + " " + "|" + " " + board[4] + " " + "|" + " " + board[5] + " " + "\n" + "-----------\n" + " " + board[6] + " " + "|" + " " + board[7] + " " + "|" + " " + board[8] + " " + "\n")};
+
+boardLayout();
